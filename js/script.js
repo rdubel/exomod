217d8bd12eaf9243c3148973d7265e61d1605812
Rémy <remdub@live.fr>
2017-02-23 09:56:44 +0100

    finished in jquery

--- js/script.js
+++ js/script.js
@@ -1,12 +1,19 @@
-var textElement = document.getElementById("text");
+// var textElement = document.getElementById("text");
+//
+// function myFunction() {
+//     var resultElement = document.getElementById("result");
+//
+//     var xd = textElement.value;
+//
+//         xd = xd.replace(/:D|:O|:\)|\(/g, "--");
+//
+//
+//     resultElement.innerHTML = xd;
+// };
 
-function myFunction() {
-    var resultElement = document.getElementById("result");
-
-    var xd = textElement.value;
-
-        xd = xd.replace(/:D|:O|:\)|\(/g, "--");
-
-
-    resultElement.innerHTML = xd;
-};
+$(document).ready(function() {
+    $("#btn").on("click", function() {
+        var text = $("#text").val().replace(/:D|:O|:\)|:\(/g, "--");
+        $("#result").html(text);
+    });
+});
